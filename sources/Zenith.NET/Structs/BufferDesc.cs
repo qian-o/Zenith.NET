34035c7e07d0353944b338d4415af1b825d9357e
王先生 <1324771795@qq.com>
2025-06-02 15:51:16 +0800

    Add BufferDesc struct to Zenith.NET namespace
    Introduces a new `BufferDesc` struct implementing the `IDesc` interface.
    This struct includes properties for buffer characteristics: `SizeInBytes`,
    `StructureStrideInBytes`, and `Flags`. A constructor initializes these
    properties to default values, and a `Validate` method checks for
    validity, ensuring that `SizeInBytes` and `StructureStrideInBytes`
    are not zero, and that `Flags` is not `BufferUsageFlags.None`.

---
+++ sources/Zenith.NET/Structs/BufferDesc.cs
@@ -0,0 +1,47 @@
+﻿namespace Zenith.NET;
+
+public record struct BufferDesc : IDesc
+{
+    public BufferDesc()
+    {
+        SizeInBytes = 0;
+        StructureStrideInBytes = 0;
+        Flags = BufferUsageFlags.Dynamic;
+    }
+
+    /// <summary>
+    /// The desired capacity, in bytes.
+    /// </summary>
+    public uint SizeInBytes { get; set; }
+
+    /// <summary>
+    /// The byte stride of the structure.
+    /// If the buffer is not structured, this should be set to 1.
+    /// </summary>
+    public uint StructureStrideInBytes { get; set; }
+
+    /// <summary>
+    /// Indicates the intended use of the buffer.
+    /// </summary>
+    public BufferUsageFlags Flags { get; set; }
+
+    public readonly bool Validate()
+    {
+        if (SizeInBytes is 0)
+        {
+            return false;
+        }
+
+        if (StructureStrideInBytes is 0)
+        {
+            return false;
+        }
+
+        if (Flags is BufferUsageFlags.None)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
