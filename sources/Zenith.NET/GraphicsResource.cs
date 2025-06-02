5c199184f178a62bf46572367dfb856ac866dd3d
王先生 <1324771795@qq.com>
2025-06-02 15:42:44 +0800

    Add preview language support and new GraphicsResource class
    Updated `Directory.Build.props` to include `<LangVersion>preview</LangVersion>` for enabling preview C# features.
    
    Introduced a new abstract class `GraphicsResource` in the `Zenith.NET` namespace, inheriting from `DisposableObject`. This class features a `Name` property with change notification and a protected `Context` property for accessing the `GraphicsContext`. An abstract method `SetName` is defined for custom implementation in derived classes.

---
+++ sources/Zenith.NET/GraphicsResource.cs
@@ -0,0 +1,22 @@
+﻿namespace Zenith.NET;
+
+public abstract class GraphicsResource(GraphicsContext context) : DisposableObject
+{
+    public string Name
+    {
+        get;
+        set
+        {
+            if (field != value)
+            {
+                field = value;
+
+                SetName(value);
+            }
+        }
+    } = string.Empty;
+
+    protected GraphicsContext Context => context;
+
+    protected abstract void SetName(string name);
+}
