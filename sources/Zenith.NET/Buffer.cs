cd3f1032ad15fb1646162240062cf01cc2352131
王先生 <1324771795@qq.com>
2025-06-02 16:28:01 +0800

    Remove unsafe block allowance and add Buffer class
    Updated `Directory.Build.props` to remove the `<AllowUnsafeBlocks>` setting, potentially impacting unsafe code usage.
    
    Introduced a new abstract `Buffer` class in `Buffer.cs` within the `Zenith.NET` namespace, inheriting from `GraphicsResource`. This class includes a constructor for `GraphicsContext` and `BufferDesc`, a property for buffer description, and an abstract property for shared access. Added a generic `Upload<T>` method for data uploads with size validation, along with an abstract `UploadImpl<T>` method for derived classes to implement.

---
+++ sources/Zenith.NET/Buffer.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Zenith.NET;
+
+public abstract class Buffer(GraphicsContext context, BufferDesc desc) : GraphicsResource(context)
+{
+    public BufferDesc Desc { get; } = desc;
+
+    /// <summary>
+    /// The CPU and GPU share access to the resource, allocated in system memory.
+    /// Only valid if the <see cref="BufferUsageFlags.Dynamic"/> flag is set.
+    /// </summary>
+    public abstract nint SharedPointer { get; }
+
+    public void Upload<T>(ReadOnlySpan<T> data, uint offsetInBytes = 0) where T : unmanaged
+    {
+        if (data.Length is 0)
+        {
+            return;
+        }
+
+        if ((uint)(data.Length * Unsafe.SizeOf<T>()) + offsetInBytes > Desc.SizeInBytes)
+        {
+            throw new ArgumentOutOfRangeException(nameof(data), "Data exceeds buffer size.");
+        }
+
+        UploadImpl(data, offsetInBytes);
+    }
+
+    protected abstract void UploadImpl<T>(ReadOnlySpan<T> data, uint offsetInBytes) where T : unmanaged;
+}
