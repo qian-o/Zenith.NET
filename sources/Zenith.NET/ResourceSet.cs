1303148a48805d2a9161619c23b0864e4e706e51
王先生 <1324771795@qq.com>
2025-06-05 18:15:26 +0800

    Add ResourceSet class in Zenith.NET namespace
    Introduces a new abstract class `ResourceSet` that inherits
    from `GraphicsResource`. This class encapsulates a group of
    resources and includes a property `Desc` for the resource
    set description, along with appropriate documentation.

---
+++ sources/Zenith.NET/ResourceSet.cs
@@ -0,0 +1,12 @@
+﻿namespace Zenith.NET;
+
+/// <summary>
+/// Represents a GPU resource set, encapsulating a group of resources to be bound together according to a specified layout.
+/// </summary>
+public abstract class ResourceSet(GraphicsContext context, ResourceSetDesc desc) : GraphicsResource(context)
+{
+    /// <summary>
+    /// Gets the resource set description.
+    /// </summary>
+    public ResourceSetDesc Desc { get; } = desc;
+}
