63ad407edda7dbdc83c6eb3fd34d3b57b280edfe
王先生 <1324771795@qq.com>
2025-06-10 14:06:38 +0800

    Add CommandBuffer and CommandQueue classes
    Introduce abstract classes `CommandBuffer` and `CommandQueue` in the `Zenith.NET` namespace. The `CommandBuffer` class includes methods for managing command buffers, while the `CommandQueue` class handles command buffer retrieval, idle waiting, and submission. Both classes implement a locking mechanism for thread safety.

---
+++ sources/Zenith.NET/CommandBuffer.cs
@@ -0,0 +1,17 @@
+﻿namespace Zenith.NET;
+
+public abstract class CommandBuffer(GraphicsContext context, CommandQueue queue) : GraphicsResource(context)
+{
+    #region Command Buffer Management
+    public abstract void Begin();
+
+    public abstract void End();
+
+    public abstract void Reset();
+
+    public void Submit()
+    {
+        queue.Submit(this);
+    }
+    #endregion
+}
---
+++ sources/Zenith.NET/CommandQueue.cs
@@ -0,0 +1,56 @@
+﻿namespace Zenith.NET;
+
+public abstract class CommandQueue(GraphicsContext context, CommandQueueType type) : GraphicsResource(context)
+{
+    private readonly Lock @lock = new();
+    private readonly List<CommandBuffer> available = [];
+    private readonly List<CommandBuffer> execution = [];
+
+    public CommandQueueType Type { get; } = type;
+
+    public CommandBuffer CommandBuffer()
+    {
+        using Lock.Scope _ = @lock.EnterScope();
+
+        CommandBuffer commandBuffer;
+
+        if (available.Count > 0)
+        {
+            commandBuffer = available[0];
+            commandBuffer.Reset();
+
+            available.RemoveAt(0);
+        }
+        else
+        {
+            commandBuffer = CreateCommandBuffer();
+        }
+
+        return commandBuffer;
+    }
+
+    public void WaitIdle()
+    {
+        using Lock.Scope _ = @lock.EnterScope();
+
+        WaitIdleImpl();
+
+        available.AddRange(execution);
+        execution.Clear();
+    }
+
+    internal void Submit(CommandBuffer commandBuffer)
+    {
+        using Lock.Scope _ = @lock.EnterScope();
+
+        SubmitImpl(commandBuffer);
+
+        execution.Add(commandBuffer);
+    }
+
+    protected abstract CommandBuffer CreateCommandBuffer();
+
+    protected abstract void WaitIdleImpl();
+
+    protected abstract void SubmitImpl(CommandBuffer commandBuffer);
+}
