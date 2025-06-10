148c7fb0863d48a7e8528e510eaaaaa26b6e66ed
王先生 <1324771795@qq.com>
2025-06-10 19:49:34 +0800

    Add ray tracing pipeline classes and validation logic
    Introduces the `Zenith.NET` namespace with an abstract class
    `RayTracingPipeline` that includes a `RayTracingPipelineDesc`
    property. Defines a new record struct `RayTracingPipelineDesc`
    implementing the `IDesc` interface, with a constructor that
    initializes properties for shaders, hit groups, resource layouts,
    and limits for recursion depth and payload sizes. Adds a
    `Validate` method to ensure the integrity of the struct's
    properties.

---
+++ sources/Zenith.NET/RayTracingPipeline.cs
@@ -0,0 +1,6 @@
+﻿namespace Zenith.NET;
+
+public abstract class RayTracingPipeline(GraphicsContext context, RayTracingPipelineDesc desc) : Pipeline(context)
+{
+    public RayTracingPipelineDesc Desc { get; } = desc;
+}
---
+++ sources/Zenith.NET/Structs/RayTracingPipelineDesc.cs
@@ -0,0 +1,63 @@
+﻿using Zenith.NET.Helpers;
+
+namespace Zenith.NET;
+
+public record struct RayTracingPipelineDesc : IDesc
+{
+    public RayTracingPipelineDesc()
+    {
+        Shaders = new();
+        HitGroups = [];
+        ResourceLayouts = [];
+        MaxTraceRecursionDepth = 6;
+        MaxPayloadSizeInBytes = 256;
+        MaxAttributeSizeInBytes = 32;
+    }
+
+    public RayTracingShadersDesc Shaders { get; set; }
+
+    public HitGroupDesc[] HitGroups { get; set; }
+
+    public ResourceLayout[] ResourceLayouts { get; set; }
+
+    public uint MaxTraceRecursionDepth { get; set; }
+
+    public uint MaxPayloadSizeInBytes { get; set; }
+
+    public uint MaxAttributeSizeInBytes { get; set; }
+
+    public readonly bool Validate()
+    {
+        if (!Shaders.Validate())
+        {
+            return false;
+        }
+
+        if (!Validation.IsValidDescs(HitGroups))
+        {
+            return false;
+        }
+
+        if (ResourceLayouts is null)
+        {
+            return false;
+        }
+
+        if (MaxTraceRecursionDepth > 31)
+        {
+            return false;
+        }
+
+        if (MaxPayloadSizeInBytes % 4 is not 0)
+        {
+            return false;
+        }
+
+        if (MaxAttributeSizeInBytes % 4 is not 0 || MaxAttributeSizeInBytes > 32)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
