3bc19a23cb86f8606aca1ce8254b9fbde1abad98
qian-o <1324771795@qq.com>
2025-06-20 13:39:05 +0800

    Refactor Upload methods to be abstract in Buffer and Texture
    Changed the `Upload` method in both the `Buffer` and `Texture` classes from concrete implementations to abstract methods. This enforces that derived classes must provide their own implementations for uploading data while retaining the same method signatures.

--- sources/Zenith.NET/Buffer.cs
+++ sources/Zenith.NET/Buffer.cs
@@ -6,15 +6,5 @@ public abstract class Buffer(GraphicsContext context, BufferDesc desc) : Graphic
 
     public abstract nint SharedPointer { get; }
 
-    public void Upload<T>(ReadOnlySpan<T> data, uint offsetInBytes = 0)
-    {
-        CommandBuffer commandBuffer = Context.Copy!.CommandBuffer();
-
-        commandBuffer.Begin();
-        commandBuffer.UpdateBuffer(this, data, offsetInBytes);
-        commandBuffer.End();
-        commandBuffer.Submit();
-
-        Context.Copy.WaitIdle();
-    }
+    public abstract void Upload<T>(ReadOnlySpan<T> data, uint offsetInBytes = 0);
 }
--- sources/Zenith.NET/Texture.cs
+++ sources/Zenith.NET/Texture.cs
@@ -4,15 +4,5 @@ public abstract class Texture(GraphicsContext context, TextureDesc desc) : Graph
 {
     public TextureDesc Desc { get; } = desc;
 
-    public void Upload<T>(ReadOnlySpan<T> data, TexturePosition position, uint width, uint height, uint depth)
-    {
-        CommandBuffer commandBuffer = Context.Copy!.CommandBuffer();
-
-        commandBuffer.Begin();
-        commandBuffer.UpdateTexture(this, data, position, width, height, depth);
-        commandBuffer.End();
-        commandBuffer.Submit();
-
-        Context.Copy.WaitIdle();
-    }
+    public abstract void Upload<T>(ReadOnlySpan<T> data, TexturePosition position, uint width, uint height, uint depth);
 }
