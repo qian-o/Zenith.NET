648bcbe30cf527d051a6311febae8a219391c308
qian-o <1324771795@qq.com>
2025-08-31 15:14:21 +0800

    Add MacOS and PixelBuffer support in SurfaceType
    - Updated `SurfaceType` enum to include `MacOS` and `PixelBuffer`.
    - Introduced a static method `PixelBuffer` in the `Surface` struct to create a `Surface` instance with pixel buffer properties.
    - Created a new `PixelBuffer` abstract class with properties for `NativePointer`, `SizeInBytes`, and an `Invalidate` method.

---
+++ sources/Zenith.NET/PixelBuffer.cs
@@ -0,0 +1,10 @@
+﻿namespace Zenith.NET;
+
+public abstract class PixelBuffer
+{
+    public abstract nint NativePointer { get; }
+
+    public abstract uint SizeInBytes { get; }
+
+    public abstract void Invalidate();
+}
