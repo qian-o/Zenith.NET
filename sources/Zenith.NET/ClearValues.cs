3bb77b8ec40aae319613675fd800506357610e17
qian-o <1324771795@qq.com>
2025-09-15 14:41:37 +0800

    Add ClearValues class for rendering clear operations
    Introduces a new static class `ClearValues` in the `Zenith.NET` namespace, defining several static readonly properties for different clear values. The `Default` clear value is set with a color array of eight `Vector4` instances, depth, stencil, and flags. Additional properties for color-only, depth-only, stencil-only, and none clear operations are also defined.

---
+++ sources/Zenith.NET/ClearValues.cs
@@ -0,0 +1,34 @@
+﻿using System.Numerics;
+
+namespace Zenith.NET;
+
+public static class ClearValues
+{
+    public static readonly ClearValue Default = new()
+    {
+        ColorValues = [.. Enumerable.Repeat<Vector4>(new(0, 0, 0, 1), 8)],
+        Depth = 1.0f,
+        Stencil = 0,
+        Flags = ClearFlags.All
+    };
+
+    public static readonly ClearValue ColorOnly = Default with
+    {
+        Flags = ClearFlags.Color
+    };
+
+    public static readonly ClearValue DepthOnly = Default with
+    {
+        Flags = ClearFlags.Depth
+    };
+
+    public static readonly ClearValue StencilOnly = Default with
+    {
+        Flags = ClearFlags.Stencil
+    };
+
+    public static readonly ClearValue None = Default with
+    {
+        Flags = ClearFlags.None
+    };
+}
