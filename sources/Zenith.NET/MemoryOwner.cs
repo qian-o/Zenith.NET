1f004067cf5dd8b48137531098f05707721f129e
qian-o <1324771795@qq.com>
2025-10-11 11:02:23 +0800

    Simplify MemoryOwner allocation logic
    Removed the custom `Alloc` method and the associated lock from the `MemoryOwner` class. The `Native` method now directly uses `NativeMemory.Alloc` for memory allocation, which simplifies the code by eliminating the need for a separate allocation method and lock management. The `allocations` list is updated directly within the `Native` method to track allocated pointers. These changes streamline the memory allocation process and improve code clarity.

--- sources/Zenith.NET/MemoryOwner.cs
+++ sources/Zenith.NET/MemoryOwner.cs
@@ -5,13 +5,14 @@ namespace Zenith.NET;
 public unsafe class MemoryOwner : DisposableObject
 {
     private readonly List<nint> allocations = [];
-    private readonly Lock @lock = new();
 
     internal nint Native<T>(ReadOnlySpan<T> data) where T : unmanaged
     {
-        void* ptr = Alloc((uint)(data.Length * sizeof(T)));
+        void* ptr = NativeMemory.Alloc((uint)(data.Length * sizeof(T)));
 
         data.CopyTo(new Span<T>(ptr, data.Length));
+
+        allocations.Add((nint)ptr);
 
         return (nint)ptr;
     }
@@ -24,16 +25,5 @@ public unsafe class MemoryOwner : DisposableObject
         }
 
         allocations.Clear();
-    }
-
-    private void* Alloc(uint sizeInBytes)
-    {
-        using Lock.Scope _ = @lock.EnterScope();
-
-        void* ptr = NativeMemory.Alloc(sizeInBytes);
-
-        allocations.Add((nint)ptr);
-
-        return ptr;
     }
 }
