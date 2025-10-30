32a4b56ec5ca6eaa0111bf07a33e1f710cb88bf4
qian-o <1324771795@qq.com>
2025-10-30 17:41:48 +0800

    Refactor Vulkan backend and add ray tracing support
    Removed `Silk.NET.Vulkan.Extensions.MVK` package, dropping MoltenVK support.
    Refactored `VKGraphicsContext` to manage Vulkan instance layers and extensions dynamically.
    Added support for validation layers and advanced Vulkan extensions, including `VK_KHR_ray_query`.
    Introduced `KhrRayQuery` static class for ray tracing integration.
    Improved code organization, readability, and modularity in Vulkan backend.

---
+++ sources/Zenith.NET.Vulkan/KhrRayQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace Zenith.NET;
+
+internal static class KhrRayQuery
+{
+    public const string ExtensionName = "VK_KHR_ray_query";
+}
--- sources/Zenith.NET.Vulkan/VKGraphicsContext.cs
+++ sources/Zenith.NET.Vulkan/VKGraphicsContext.cs
@@ -1,7 +1,58 @@
-﻿namespace Zenith.NET;
+﻿using System.Runtime.CompilerServices;
+using Silk.NET.Core;
+using Silk.NET.Core.Contexts;
+using Silk.NET.Vulkan;
+using Silk.NET.Vulkan.Extensions.EXT;
+using Silk.NET.Vulkan.Extensions.KHR;
 
-internal class VKGraphicsContext(bool useValidationLayer) : GraphicsContext(Backend.Vulkan, useValidationLayer)
+namespace Zenith.NET;
+
+internal unsafe class VKGraphicsContext(bool useValidationLayer) : GraphicsContext(Backend.Vulkan, useValidationLayer)
 {
+    private static readonly string[] InstanceLayers =
+    [
+        "VK_LAYER_KHRONOS_validation"
+    ];
+
+    private static readonly string[] InstanceExtensions =
+    [
+        ExtDebugUtils.ExtensionName,
+        KhrSurface.ExtensionName,
+        KhrWin32Surface.ExtensionName,
+        KhrWaylandSurface.ExtensionName,
+        KhrXlibSurface.ExtensionName,
+        KhrAndroidSurface.ExtensionName,
+        ExtMetalSurface.ExtensionName
+    ];
+
+    private static readonly string[] DeviceExtensions =
+    [
+        KhrSwapchain.ExtensionName,
+        KhrExternalMemoryWin32.ExtensionName,
+        KhrRayQuery.ExtensionName,
+        KhrRayTracingPipeline.ExtensionName,
+        KhrAccelerationStructure.ExtensionName,
+        KhrDeferredHostOperations.ExtensionName
+    ];
+
+    public VkInstance Instance;
+
+    public Vk Vk { get; } = Vk.GetApi();
+
+    public ExtDebugUtils? DebugUtils { get; private set; }
+
+    public KhrSurface? Surface { get; private set; }
+
+    public KhrWin32Surface? Win32Surface { get; private set; }
+
+    public KhrWaylandSurface? WaylandSurface { get; private set; }
+
+    public KhrXlibSurface? XlibSurface { get; private set; }
+
+    public KhrAndroidSurface? AndroidSurface { get; private set; }
+
+    public ExtMetalSurface? MetalSurface { get; private set; }
+
     protected override void Initialize(bool useValidationLayer,
                                        out Capabilities capabilities,
                                        out CommandQueue direct,
@@ -9,6 +60,65 @@ internal class VKGraphicsContext(bool useValidationLayer) : GraphicsContext(Back
                                        out CommandQueue copy,
                                        out ValidationLayer? validationLayer)
     {
+        using ZenithMarshal.Scope scope = new();
+
+        // Create instance
+        {
+            uint extensionCount = 0;
+            Vk.EnumerateInstanceExtensionProperties((byte*)null, &extensionCount, (ExtensionProperties*)null);
+
+            ExtensionProperties* availableExtensions = (ExtensionProperties*)ZenithMarshal.Allocate<ExtensionProperties>(scope, extensionCount);
+            Vk.EnumerateInstanceExtensionProperties((byte*)null, &extensionCount, availableExtensions);
+
+            string[] instanceExtensions = [.. new ReadOnlySpan<ExtensionProperties>(availableExtensions, (int)extensionCount).ToArray().Select(static item => ZenithMarshal.StringFromPointer((nint)item.ExtensionName, StringEncoding.UTF8))];
+            instanceExtensions = [.. instanceExtensions.Intersect(InstanceExtensions)];
+
+            ApplicationInfo ApplicationInfo = new()
+            {
+                SType = StructureType.ApplicationInfo,
+                PApplicationName = (byte*)ZenithMarshal.StringToPointer(scope, AppDomain.CurrentDomain.FriendlyName, StringEncoding.UTF8),
+                ApplicationVersion = new Version32(1, 0, 0),
+                PEngineName = (byte*)ZenithMarshal.StringToPointer(scope, "Zenith.NET", StringEncoding.UTF8),
+                EngineVersion = new Version32(1, 0, 0),
+                ApiVersion = Vk.Version13
+            };
+
+            InstanceCreateInfo createInfo = new()
+            {
+                SType = StructureType.InstanceCreateInfo,
+                PApplicationInfo = &ApplicationInfo,
+                EnabledExtensionCount = (uint)instanceExtensions.Length,
+                PpEnabledExtensionNames = (byte**)ZenithMarshal.StringArrayToPointer(scope, instanceExtensions, StringEncoding.UTF8)
+            };
+
+            if (useValidationLayer)
+            {
+                uint layerCount = 0;
+                Vk.EnumerateInstanceLayerProperties(&layerCount, (LayerProperties*)null);
+
+                LayerProperties* availableLayers = (LayerProperties*)ZenithMarshal.Allocate<LayerProperties>(scope, layerCount);
+                Vk.EnumerateInstanceLayerProperties(&layerCount, availableLayers);
+
+                string[] validationLayers = [.. new ReadOnlySpan<LayerProperties>(availableLayers, (int)layerCount).ToArray().Select(static item => ZenithMarshal.StringFromPointer((nint)item.LayerName, StringEncoding.UTF8))];
+                validationLayers = [.. validationLayers.Intersect(InstanceLayers)];
+
+                createInfo.EnabledLayerCount = (uint)validationLayers.Length;
+                createInfo.PpEnabledLayerNames = (byte**)ZenithMarshal.StringArrayToPointer(scope, validationLayers, StringEncoding.UTF8);
+            }
+
+            Vk.CreateInstance(&createInfo, null, (VkInstance*)Unsafe.AsPointer(ref Instance));
+
+            LamdaNativeContext context = new((proc) => Vk.GetInstanceProcAddr(Instance, (byte*)ZenithMarshal.StringToPointer(scope, proc, StringEncoding.UTF8)));
+
+            DebugUtils = instanceExtensions.Contains(ExtDebugUtils.ExtensionName) ? new(context) : null;
+            Surface = instanceExtensions.Contains(KhrSurface.ExtensionName) ? new(context) : null;
+            Win32Surface = instanceExtensions.Contains(KhrWin32Surface.ExtensionName) ? new(context) : null;
+            WaylandSurface = instanceExtensions.Contains(KhrWaylandSurface.ExtensionName) ? new(context) : null;
+            XlibSurface = instanceExtensions.Contains(KhrXlibSurface.ExtensionName) ? new(context) : null;
+            AndroidSurface = instanceExtensions.Contains(KhrAndroidSurface.ExtensionName) ? new(context) : null;
+            MetalSurface = instanceExtensions.Contains(ExtMetalSurface.ExtensionName) ? new(context) : null;
+        }
+
         throw new NotImplementedException();
     }
 
