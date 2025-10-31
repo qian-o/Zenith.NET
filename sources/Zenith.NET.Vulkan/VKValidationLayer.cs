5301a594edea189e0e536e33642fe71da275d8b6
qian-o <1324771795@qq.com>
2025-10-31 14:18:50 +0800

    Add Vulkan debug messenger to VKValidationLayer
    Implemented Vulkan debug messenger creation and destruction in the `VKValidationLayer` class using the `DebugUtils` extension. Added a `UserCallback` method to handle debug messages and report them with appropriate severity. Introduced a new `Context` property for casting to `VKGraphicsContext`. Updated the `SetResourceName` and `Destroy` methods, replacing `NotImplementedException` placeholders. Added `System.Runtime.CompilerServices` namespace for pointer manipulation support.

--- sources/Zenith.NET.Vulkan/VKValidationLayer.cs
+++ sources/Zenith.NET.Vulkan/VKValidationLayer.cs
@@ -1,18 +1,53 @@
-﻿namespace Zenith.NET;
+﻿using System.Runtime.CompilerServices;
+using Silk.NET.Vulkan;
+
+namespace Zenith.NET;
 
 internal unsafe class VKValidationLayer : ValidationLayer
 {
+    private readonly DebugUtilsMessengerEXT messenger;
+
     public VKValidationLayer(GraphicsContext context) : base(context)
     {
+        DebugUtilsMessengerCreateInfoEXT createInfo = new()
+        {
+            SType = StructureType.DebugUtilsMessengerCreateInfoExt,
+            MessageSeverity = DebugUtilsMessageSeverityFlagsEXT.VerboseBitExt
+                              | DebugUtilsMessageSeverityFlagsEXT.InfoBitExt
+                              | DebugUtilsMessageSeverityFlagsEXT.WarningBitExt
+                              | DebugUtilsMessageSeverityFlagsEXT.ErrorBitExt,
+            MessageType = DebugUtilsMessageTypeFlagsEXT.GeneralBitExt
+                          | DebugUtilsMessageTypeFlagsEXT.ValidationBitExt
+                          | DebugUtilsMessageTypeFlagsEXT.PerformanceBitExt,
+            PfnUserCallback = new(UserCallback)
+        };
+
+        Context.DebugUtils?.CreateDebugUtilsMessenger(Context.Instance, &createInfo, null, (DebugUtilsMessengerEXT*)Unsafe.AsPointer(ref messenger)).Success();
     }
+
+    public new VKGraphicsContext Context => (VKGraphicsContext)base.Context;
 
     protected override void SetResourceName(string name)
     {
-        throw new NotImplementedException();
     }
 
     protected override void Destroy()
     {
-        throw new NotImplementedException();
+        Context.DebugUtils?.DestroyDebugUtilsMessenger(Context.Instance, messenger, null);
+    }
+
+    private uint UserCallback(DebugUtilsMessageSeverityFlagsEXT messageSeverity,
+                              DebugUtilsMessageTypeFlagsEXT messageTypes,
+                              DebugUtilsMessengerCallbackDataEXT* pCallbackData,
+                              void* pUserData)
+    {
+        Report(MessageSource.GraphicsAPI, messageSeverity switch
+        {
+            DebugUtilsMessageSeverityFlagsEXT.ErrorBitExt => MessageSeverity.Error,
+            DebugUtilsMessageSeverityFlagsEXT.WarningBitExt => MessageSeverity.Warning,
+            _ => MessageSeverity.Message
+        }, ZenithMarshal.StringFromPointer((nint)pCallbackData->PMessage, StringEncoding.UTF8));
+
+        return Vk.False;
     }
 }
