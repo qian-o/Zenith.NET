3952d445ec2d2600541539d43edee959617085f5
qian-o <1324771795@qq.com>
2025-11-03 17:10:59 +0800

    Add Vulkan buffer view support and resource management
    Enhanced Vulkan integration by adding `VkBufferView` support:
    - Introduced a global using directive for `VkBufferView`.
    - Updated `VKBufferView` to include Vulkan-specific functionality.
    - Added an unsafe `BufferView` field and initialized it using Vulkan's `CreateBufferView`.
    - Implemented resource naming with `DebugUtilsObjectNameInfoEXT` for debugging.
    - Added cleanup logic to destroy `BufferView` resources properly.
    
    Removed unused `BufferInfo` property and redundant imports. Improved code structure for better Vulkan resource handling.

--- sources/Zenith.NET.Vulkan/VKBufferView.cs
+++ sources/Zenith.NET.Vulkan/VKBufferView.cs
@@ -1,26 +1,45 @@
-﻿using Silk.NET.Vulkan;
+﻿using System.Runtime.CompilerServices;
+using Silk.NET.Vulkan;
 
 namespace Zenith.NET;
 
-internal class VKBufferView : BufferView
+internal unsafe class VKBufferView : BufferView
 {
+    public VkBufferView BufferView;
+
     public VKBufferView(GraphicsContext context, BufferViewDesc desc) : base(context, desc)
     {
-        BufferInfo = new()
+        BufferViewCreateInfo createInfo = new()
         {
+            SType = StructureType.BufferViewCreateInfo,
             Buffer = Desc.Buffer.Vulkan().Buffer,
+            Format = Format.Undefined,
             Offset = Desc.OffsetInBytes,
             Range = Desc.SizeInBytes
         };
+
+        Context.Vk.CreateBufferView(Context.Device, &createInfo, null, (VkBufferView*)Unsafe.AsPointer(ref BufferView)).Success();
     }
 
-    public DescriptorBufferInfo BufferInfo { get; }
+    public new VKGraphicsContext Context => (VKGraphicsContext)base.Context;
 
     protected override void SetResourceName(string name)
     {
+        using ZenithMarshal.Scope scope = new();
+
+        DebugUtilsObjectNameInfoEXT nameInfo = new()
+        {
+            SType = StructureType.DebugUtilsObjectNameInfoExt,
+            ObjectType = ObjectType.BufferView,
+            ObjectHandle = BufferView.Handle,
+            PObjectName = (byte*)ZenithMarshal.StringToPointer(scope, name, StringEncoding.UTF8)
+        };
+
+        Context.DebugUtils?.SetDebugUtilsObjectName(Context.Device, &nameInfo).Success();
     }
 
     protected override void Destroy()
     {
+        Context.Vk.DestroyBufferView(Context.Device, BufferView, null);
     }
 }
