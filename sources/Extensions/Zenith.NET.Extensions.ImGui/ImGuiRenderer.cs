b60cf727ae3d1d7dddc08766e36bbc9b432e286d
qian-o <1324771795@qq.com>
2025-11-14 22:49:20 +0800

    Add NotImplementedException placeholders to ImGuiRenderer
    Added `NotImplementedException` to the constructor, `Render`,
    and `Destroy` methods of the `ImGuiRenderer` class in the
    `Zenith.NET.Extensions.ImGui` namespace. These placeholders
    indicate that the methods are not yet implemented and will
    be completed in the future.

--- sources/Extensions/Zenith.NET.Extensions.ImGui/ImGuiRenderer.cs
+++ sources/Extensions/Zenith.NET.Extensions.ImGui/ImGuiRenderer.cs
@@ -6,13 +6,16 @@ internal class ImGuiRenderer : DisposableObject
 {
     public ImGuiRenderer(GraphicsContext context, Output output, ImGuiColorSpace colorSpace)
     {
+        throw new NotImplementedException();
     }
 
     public void Render(CommandBuffer commandBuffer, ImDrawDataPtr drawData)
     {
+        throw new NotImplementedException();
     }
 
     protected override void Destroy()
     {
+        throw new NotImplementedException();
     }
 }
