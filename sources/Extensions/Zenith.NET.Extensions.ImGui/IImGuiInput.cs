28c0647de8c6fdabb69d19abce2093056372cec9
qian-o <1324771795@qq.com>
2025-11-14 17:39:07 +0800

    Add ImGui input interface, color space enum, and controller
    Introduced `IImGuiInput` interface to handle ImGui input events,
    including mouse, keyboard, and character input. Added the
    `ImGuiColorSpace` enum to represent color spaces (`Legacy` and
    `Linear`) for rendering.
    
    Implemented `ImGuiController` to manage the ImGui context and
    configuration. This includes initializing the context, setting
    IO flags, optionally loading fonts, and supporting additional
    custom setup. Proper cleanup of the ImGui context is handled
    in the `Destroy` method.

---
+++ sources/Extensions/Zenith.NET.Extensions.ImGui/IImGuiInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+using Hexa.NET.ImGui;
+
+namespace Zenith.NET.Extensions.ImGui;
+
+public interface IImGuiInput
+{
+    ImGuiMouseCursor Cursor { get; set; }
+
+    event Action<ImGuiMouseButton> OnMouseDown;
+
+    event Action<ImGuiMouseButton> OnMouseUp;
+
+    event Action<Vector2> OnMouseMove;
+
+    event Action<Vector2> OnMouseWheel;
+
+    event Action<ImGuiKey> OnKeyDown;
+
+    event Action<ImGuiKey> OnKeyUp;
+
+    event Action<char> OnCharInput;
+}
---
+++ sources/Extensions/Zenith.NET.Extensions.ImGui/ImGuiController.cs
@@ -0,0 +1,42 @@
+﻿using Hexa.NET.ImGui;
+using HexaImGui = Hexa.NET.ImGui.ImGui;
+
+namespace Zenith.NET.Extensions.ImGui;
+
+public unsafe class ImGuiController : DisposableObject
+{
+    public ImGuiController(GraphicsContext context,
+                           IImGuiInput input,
+                           Output output,
+                           ImGuiColorSpace colorSpace = ImGuiColorSpace.Legacy,
+                           string? fontPath = null,
+                           Action<ImGuiIOPtr>? otherSetup = null)
+    {
+        HexaImGui.SetCurrentContext(Handle = HexaImGui.CreateContext());
+
+        ImGuiIOPtr io = HexaImGui.GetIO();
+
+        io.ConfigFlags |= ImGuiConfigFlags.NavEnableKeyboard;
+
+        io.BackendFlags |= ImGuiBackendFlags.HasMouseCursors;
+        io.BackendFlags |= ImGuiBackendFlags.RendererHasVtxOffset;
+        io.BackendFlags |= ImGuiBackendFlags.RendererHasTextures;
+
+        if (fontPath is not null)
+        {
+            io.Fonts.Clear();
+
+            io.Fonts.AddFontFromFileTTF(fontPath);
+        }
+
+        otherSetup?.Invoke(io);
+    }
+
+    public ImGuiContextPtr Handle { get; }
+
+    protected override void Destroy()
+    {
+        HexaImGui.SetCurrentContext(null);
+        HexaImGui.DestroyContext(Handle);
+    }
+}
