d12bcc72821e36399e2384557f89c48248e48415
qian-o <1324771795@qq.com>
2025-11-14 15:19:00 +0800

    Add shader compilation extension for GraphicsContext
    Introduced a new `Extensions` class in the `Zenith.NET.Extensions.Slang` namespace, providing an extension method for `GraphicsContext`.
    
    Implemented the `CompileShader` method to enable shader compilation using the Slang compiler, with backend-specific argument adjustments for DirectX12, Metal, and Vulkan.
    
    Added a placeholder `ReflectShaderLayout` method for future shader reflection functionality.

---
+++ sources/Extensions/Zenith.NET.Extensions.Slang/Extensions.cs
@@ -0,0 +1,43 @@
+﻿using Slangc.NET;
+
+namespace Zenith.NET.Extensions.Slang;
+
+public static class Extensions
+{
+    extension(GraphicsContext context)
+    {
+        public Shader CompileShader(string filePath, string entryPoint, ShaderStageFlags stage)
+        {
+            List<string> arguments =
+            [
+                filePath,
+                "-entry", entryPoint,
+                "-stage", stage.ToString().ToLowerInvariant(),
+                "-matrix-layout-row-major",
+                "-preserve-params"
+            ];
+
+            switch (context.Backend)
+            {
+                case Backend.DirectX12:
+                    arguments.AddRange(["-target", "dxil"]);
+                    break;
+
+                case Backend.Metal:
+                    arguments.AddRange(["-target", "metal"]);
+                    break;
+
+                case Backend.Vulkan:
+                    arguments.AddRange(["-fvk-use-dx-layout", "-fvk-use-entrypoint-name", "-target", "spirv"]);
+                    break;
+            }
+
+            return context.CreateShader(new() { ShaderBytes = SlangCompiler.Compile([.. arguments]), EntryPoint = entryPoint, Stage = stage });
+        }
+
+        public void ReflectShaderLayout()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
