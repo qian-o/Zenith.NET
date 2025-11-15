c05cd642ff3931889bc4ec46892293b5130b2023
qian-o <1324771795@qq.com>
2025-11-15 21:01:47 +0800

    Expand TextureUsageFlags check for SampledBit usage
    Updated the condition in `VKFormats` to include `TextureUsageFlags.Dynamic` alongside `TextureUsageFlags.ShaderResource` when determining if `ImageUsageFlags.SampledBit` should be added. This ensures proper handling of dynamic texture usage scenarios.

--- sources/Zenith.NET.Vulkan/VKFormats.cs
+++ sources/Zenith.NET.Vulkan/VKFormats.cs
@@ -255,7 +255,7 @@ internal static class VKFormats
             imageUsageFlags |= ImageUsageFlags.DepthStencilAttachmentBit;
         }
 
-        if (textureUsageFlags.HasFlag(TextureUsageFlags.ShaderResource))
+        if (textureUsageFlags.HasFlag(TextureUsageFlags.ShaderResource) || textureUsageFlags.HasFlag(TextureUsageFlags.Dynamic))
         {
             imageUsageFlags |= ImageUsageFlags.SampledBit;
         }
