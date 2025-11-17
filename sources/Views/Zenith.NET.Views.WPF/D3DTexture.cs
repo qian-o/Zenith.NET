68b9ed5cf51555f05c3cfe0fa66debae194a5115
qian-o <1324771795@qq.com>
2025-11-17 10:24:48 +0800

    Add Direct3D-based rendering to ZenithView control
    Introduce a `D3DImage`-based rendering mechanism in `ZenithView.cs`
    to enable GPU-accelerated rendering in WPF. Added a `D3DTexture`
    class to manage Direct3D9 and Direct3D11 texture interoperation
    using the Silk.NET library.
    
    `ZenithView` now handles resizing events by creating and associating
    a `D3DTexture` with the `D3DImage` back buffer. Removed unused
    event declarations and commented-out code.
    
    The `D3DTexture` class encapsulates Direct3D resource management,
    including texture creation, shared handle retrieval, and proper
    resource disposal. These changes enable efficient rendering
    capabilities for the `ZenithView` control.

---
+++ sources/Views/Zenith.NET.Views.WPF/D3DTexture.cs
@@ -0,0 +1,99 @@
+﻿using System.Windows;
+using System.Windows.Interop;
+using Silk.NET.Core.Native;
+using Silk.NET.Direct3D11;
+using Silk.NET.Direct3D9;
+using Silk.NET.DXGI;
+using PresentParameters = Silk.NET.Direct3D9.PresentParameters;
+
+namespace Zenith.NET.Views.WPF;
+
+internal unsafe class D3DTexture : DisposableObject
+{
+    public static ComPtr<IDirect3D9Ex> D3D9Ex;
+
+    public static ComPtr<IDirect3DDevice9Ex> D3D9DeviceEx;
+
+    public static ComPtr<ID3D11Device> D3D11Device;
+
+    public static ComPtr<ID3D11DeviceContext> D3D11DeviceContext;
+
+    public ComPtr<IDirect3DTexture9> D3D9Texture;
+
+    public ComPtr<IDirect3DSurface9> D3D9Surface;
+
+    public ComPtr<ID3D11Texture2D> D3D11Texture;
+
+    public nint Handle;
+
+    public nint ShareHandle;
+
+    static D3DTexture()
+    {
+        D3D9 = D3D9.GetApi(null);
+        D3D11 = D3D11.GetApi(null);
+
+        D3D9.Direct3DCreate9Ex(D3D9.SdkVersion, ref D3D9Ex);
+
+        PresentParameters present = new()
+        {
+            SwapEffect = Swapeffect.Discard,
+            Windowed = true,
+            PresentationInterval = D3D9.PresentIntervalImmediate
+        };
+
+        D3D9Ex.CreateDeviceEx(0,
+                              Devtype.Hal,
+                              new WindowInteropHelper(Application.Current.MainWindow).Handle,
+                              D3D9.CreateHardwareVertexprocessing,
+                              &present,
+                              (Displaymodeex*)null,
+                              ref D3D9DeviceEx);
+
+        D3D11.CreateDevice(default(ComPtr<IDXGIAdapter>),
+                           D3DDriverType.Hardware,
+                           0,
+                           (uint)CreateDeviceFlag.BgraSupport,
+                           null,
+                           0,
+                           D3D11.SdkVersion,
+                           ref D3D11Device,
+                           null,
+                           ref D3D11DeviceContext);
+    }
+
+    public D3DTexture(uint width, uint height)
+    {
+        void* d3d9ShareHandle = null;
+        D3D9DeviceEx.CreateTexture(width,
+                                   height,
+                                   1,
+                                   D3D9.UsageRendertarget,
+                                   Silk.NET.Direct3D9.Format.X8R8G8B8,
+                                   Pool.Default,
+                                   ref D3D9Texture,
+                                   &d3d9ShareHandle);
+
+        D3D9Texture.GetSurfaceLevel(0, ref D3D9Surface);
+
+        D3D11Device.OpenSharedResource(d3d9ShareHandle, out D3D11Texture);
+
+        void* d3d11ShareHandle = null;
+        using ComPtr<IDXGIResource> resource = D3D11Texture.QueryInterface<IDXGIResource>();
+        resource.GetSharedHandle(&d3d11ShareHandle);
+
+        Handle = (nint)D3D9Surface.Handle;
+        ShareHandle = (nint)d3d11ShareHandle;
+    }
+
+    public static D3D9 D3D9 { get; }
+
+    public static D3D11 D3D11 { get; }
+
+    protected override void Destroy()
+    {
+        D3D11Texture.Dispose();
+        D3D9Surface.Dispose();
+        D3D9Texture.Dispose();
+    }
+}
--- sources/Views/Zenith.NET.Views.WPF/ZenithView.cs
+++ sources/Views/Zenith.NET.Views.WPF/ZenithView.cs
@@ -1,14 +1,34 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
 
 namespace Zenith.NET.Views.WPF;
 
 public class ZenithView : Control
 {
-    //public event EventHandler<ZenithUpdateEventArgs>? UpdateRequested;
+    private readonly D3DImage image;
 
-    //public event EventHandler<ZenithRenderEventArgs>? RenderRequested;
+    private D3DTexture? texture;
 
-    //public void Initialize(GraphicsContext context)
-    //{
-    //}
+    public ZenithView()
+    {
+        image = new();
+    }
+
+    protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+    {
+        base.OnRenderSizeChanged(sizeInfo);
+
+        if (ActualWidth <= 0 || ActualHeight <= 0)
+        {
+            return;
+        }
+
+        texture?.Dispose();
+        texture = new((uint)ActualWidth, (uint)ActualHeight);
+
+        image.Lock();
+        image.SetBackBuffer(D3DResourceType.IDirect3DSurface9, texture.Handle);
+        image.Unlock();
+    }
 }
