a4427290d97a97ede5898a9f6c66968f97e826b5
qian-o <1324771795@qq.com>
2025-11-17 10:47:22 +0800

    Refactor Direct3D resource management
    Refactored `D3DTexture` to delegate Direct3D resource management to a new `GPU` class, improving modularity and centralizing initialization logic. Updated `D3DTexture` to use `GPU` for resource access and added `Width` and `Height` properties.
    
    Enhanced `ZenithView` with a `GraphicsContext` dependency property for better rendering pipeline integration. Introduced an `Initialize` method to manage `SwapChain` creation and updated `OnRenderSizeChanged` to handle texture resizing. Added a placeholder `OnRender` method for future rendering logic.

--- sources/Views/Zenith.NET.Views.WPF/D3DTexture.cs
+++ sources/Views/Zenith.NET.Views.WPF/D3DTexture.cs
@@ -10,13 +10,54 @@ namespace Zenith.NET.Views.WPF;
 
 internal unsafe class D3DTexture : DisposableObject
 {
-    public static ComPtr<IDirect3D9Ex> D3D9Ex;
+    public static class GPU
+    {
+        public static ComPtr<IDirect3D9Ex> D3D9Ex;
 
-    public static ComPtr<IDirect3DDevice9Ex> D3D9DeviceEx;
+        public static ComPtr<IDirect3DDevice9Ex> D3D9DeviceEx;
 
-    public static ComPtr<ID3D11Device> D3D11Device;
+        public static ComPtr<ID3D11Device> D3D11Device;
 
-    public static ComPtr<ID3D11DeviceContext> D3D11DeviceContext;
+        public static ComPtr<ID3D11DeviceContext> D3D11DeviceContext;
+
+        static GPU()
+        {
+            D3D9 = D3D9.GetApi(null);
+            D3D11 = D3D11.GetApi(null);
+
+            D3D9.Direct3DCreate9Ex(D3D9.SdkVersion, ref D3D9Ex);
+
+            PresentParameters present = new()
+            {
+                SwapEffect = Swapeffect.Discard,
+                Windowed = true,
+                PresentationInterval = D3D9.PresentIntervalImmediate
+            };
+
+            D3D9Ex.CreateDeviceEx(0,
+                                  Devtype.Hal,
+                                  new WindowInteropHelper(Application.Current.MainWindow).Handle,
+                                  D3D9.CreateHardwareVertexprocessing,
+                                  &present,
+                                  (Displaymodeex*)null,
+                                  ref D3D9DeviceEx);
+
+            D3D11.CreateDevice(default(ComPtr<IDXGIAdapter>),
+                               D3DDriverType.Hardware,
+                               0,
+                               (uint)CreateDeviceFlag.BgraSupport,
+                               null,
+                               0,
+                               D3D11.SdkVersion,
+                               ref D3D11Device,
+                               null,
+                               ref D3D11DeviceContext);
+        }
+
+        public static D3D9 D3D9 { get; }
+
+        public static D3D11 D3D11 { get; }
+    }
 
     public ComPtr<IDirect3DTexture9> D3D9Texture;
 
@@ -26,69 +67,38 @@ internal unsafe class D3DTexture : DisposableObject
 
     public nint Handle;
 
-    public nint ShareHandle;
-
-    static D3DTexture()
-    {
-        D3D9 = D3D9.GetApi(null);
-        D3D11 = D3D11.GetApi(null);
-
-        D3D9.Direct3DCreate9Ex(D3D9.SdkVersion, ref D3D9Ex);
-
-        PresentParameters present = new()
-        {
-            SwapEffect = Swapeffect.Discard,
-            Windowed = true,
-            PresentationInterval = D3D9.PresentIntervalImmediate
-        };
-
-        D3D9Ex.CreateDeviceEx(0,
-                              Devtype.Hal,
-                              new WindowInteropHelper(Application.Current.MainWindow).Handle,
-                              D3D9.CreateHardwareVertexprocessing,
-                              &present,
-                              (Displaymodeex*)null,
-                              ref D3D9DeviceEx);
-
-        D3D11.CreateDevice(default(ComPtr<IDXGIAdapter>),
-                           D3DDriverType.Hardware,
-                           0,
-                           (uint)CreateDeviceFlag.BgraSupport,
-                           null,
-                           0,
-                           D3D11.SdkVersion,
-                           ref D3D11Device,
-                           null,
-                           ref D3D11DeviceContext);
-    }
+    public nint SharedHandle;
 
     public D3DTexture(uint width, uint height)
     {
         void* d3d9ShareHandle = null;
-        D3D9DeviceEx.CreateTexture(width,
-                                   height,
-                                   1,
-                                   D3D9.UsageRendertarget,
-                                   Silk.NET.Direct3D9.Format.X8R8G8B8,
-                                   Pool.Default,
-                                   ref D3D9Texture,
-                                   &d3d9ShareHandle);
+        GPU.D3D9DeviceEx.CreateTexture(width,
+                                       height,
+                                       1,
+                                       D3D9.UsageRendertarget,
+                                       Silk.NET.Direct3D9.Format.X8R8G8B8,
+                                       Pool.Default,
+                                       ref D3D9Texture,
+                                       &d3d9ShareHandle);
 
         D3D9Texture.GetSurfaceLevel(0, ref D3D9Surface);
 
-        D3D11Device.OpenSharedResource(d3d9ShareHandle, out D3D11Texture);
+        GPU.D3D11Device.OpenSharedResource(d3d9ShareHandle, out D3D11Texture);
 
-        void* d3d11ShareHandle = null;
+        void* d3d11SharedHandle = null;
         using ComPtr<IDXGIResource> resource = D3D11Texture.QueryInterface<IDXGIResource>();
-        resource.GetSharedHandle(&d3d11ShareHandle);
+        resource.GetSharedHandle(&d3d11SharedHandle);
 
         Handle = (nint)D3D9Surface.Handle;
-        ShareHandle = (nint)d3d11ShareHandle;
+        SharedHandle = (nint)d3d11SharedHandle;
+
+        Width = width;
+        Height = height;
     }
 
-    public static D3D9 D3D9 { get; }
+    public uint Width { get; }
 
-    public static D3D11 D3D11 { get; }
+    public uint Height { get; }
 
     protected override void Destroy()
     {
--- sources/Views/Zenith.NET.Views.WPF/ZenithView.cs
+++ sources/Views/Zenith.NET.Views.WPF/ZenithView.cs
@@ -1,18 +1,31 @@
 ﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Interop;
+using System.Windows.Media;
 
 namespace Zenith.NET.Views.WPF;
 
 public class ZenithView : Control
 {
-    private readonly D3DImage image;
+    public static readonly DependencyProperty GraphicsContextProperty = DependencyProperty.Register(nameof(GraphicsContext),
+                                                                                                    typeof(GraphicsContext),
+                                                                                                    typeof(ZenithView),
+                                                                                                    new PropertyMetadata(null, (d, _) => ((ZenithView)d).Initialize()));
 
-    private D3DTexture? texture;
+    private readonly D3DImage image = new();
+
+    private D3DTexture texture = new(100, 100);
+    private SwapChain? swapChain;
 
     public ZenithView()
     {
-        image = new();
+        Initialize();
+    }
+
+    public GraphicsContext? GraphicsContext
+    {
+        get => (GraphicsContext?)GetValue(GraphicsContextProperty);
+        set => SetValue(GraphicsContextProperty, value);
     }
 
     protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
@@ -30,5 +43,30 @@ public class ZenithView : Control
         image.Lock();
         image.SetBackBuffer(D3DResourceType.IDirect3DSurface9, texture.Handle);
         image.Unlock();
+
+        Initialize();
+    }
+
+    protected override void OnRender(DrawingContext drawingContext)
+    {
+    }
+
+    private void Initialize()
+    {
+        swapChain?.Dispose();
+
+        if (GraphicsContext is null)
+        {
+            return;
+        }
+
+        swapChain = GraphicsContext.CreateSwapChain(new()
+        {
+            Surface = Surface.D3D11Interop(texture.SharedHandle, texture.Width, texture.Height),
+            ColorTargetFormat = PixelFormat.B8G8R8A8UNorm,
+            DepthStencilTargetFormat = PixelFormat.D24UNormS8UInt
+        });
+
+        InvalidateVisual();
     }
 }
