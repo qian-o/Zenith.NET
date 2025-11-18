479c01c79e47e5ca4a72ec5bdd987c426cd7b9a6
qian-o <1324771795@qq.com>
2025-11-18 21:52:56 +0800

    Add support for event-based command bindings in WPF
    Integrated `Microsoft.Xaml.Behaviors.Wpf` to enable behaviors
    and triggers in the WPF application. Updated `Directory.Packages.props`
    and `Sample.WPF.csproj` to include the package.
    
    Enhanced `MainViewModel` with `Update` and `Render` methods,
    decorated with `[RelayCommand]` attributes for command binding.
    The `Render` method includes rendering logic using a `CommandBuffer`.
    
    Modified `MainView.xaml` to add `b:Interaction.Triggers` for
    handling `Update` and `Render` events, binding them to the
    corresponding commands in `MainViewModel`.
    
    These changes improve the application's interactivity and
    rendering capabilities.

--- sources/Experiments/Views/Sample.WPF/ViewModels/MainViewModel.cs
+++ sources/Experiments/Views/Sample.WPF/ViewModels/MainViewModel.cs
@@ -1,5 +1,26 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Zenith.NET;
+using Zenith.NET.Views.WPF;
 
 namespace Sample.WPF.ViewModels;
 
-public partial class MainViewModel : ObservableRecipient;
+public partial class MainViewModel : ObservableRecipient
+{
+    [RelayCommand]
+    private void Update(UpdateEventArgs args)
+    {
+        // Method implementation
+    }
+
+    [RelayCommand]
+    private void Render(RenderEventArgs args)
+    {
+        CommandBuffer commandBuffer = App.Context.Graphics.CommandBuffer();
+
+        commandBuffer.BindFrameBuffer(args.FrameBuffer, ClearValues.Default);
+        commandBuffer.Submit();
+
+        App.Context.Graphics.WaitIdle();
+    }
+}
