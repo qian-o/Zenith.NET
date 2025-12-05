7b622c55fa1b6e95993f57a8826b5c15b4049108
qian-o <1324771795@qq.com>
2025-12-05 17:47:57 +0800

    Add NewSponza class and Vertex struct for scene setup
    Added `NewSponza.cs` with an `internal` class `NewSponza` in the `SponzaScene.Helpers` namespace. This class provides functionality to initialize and load a model from a specified directory.
    
    Added `Vertex.cs` with an `internal` struct `Vertex` in the `SponzaScene.Models` namespace. The struct defines vertex attributes (`Position`, `Normal`, `TexCoord`, `Color`) and a static method `InputLayout` to define the vertex data layout.
    
    Both files include dependencies on `System.Numerics` and `Zenith.NET`.

---
+++ sources/Experiments/SponzaScene/Helpers/NewSponza.cs
@@ -0,0 +1,18 @@
+﻿using Zenith.NET;
+
+namespace SponzaScene.Helpers;
+
+internal class NewSponza(GraphicsContext context)
+{
+    public const string Directory = @"C:\Users\13247\OneDrive\NewSponza";
+
+    public void Initialize()
+    {
+        LoadModel("NewSponza_Main");
+    }
+
+    private void LoadModel(string name)
+    {
+        string path = Path.Combine(Directory, name);
+    }
+}
---
+++ sources/Experiments/SponzaScene/Models/Vertex.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+using Zenith.NET;
+
+namespace SponzaScene.Models;
+
+internal struct Vertex
+{
+    public Vector3 Position;
+
+    public Vector3 Normal;
+
+    public Vector2 TexCoord;
+
+    public Vector3 Color;
+
+    public static InputLayout InputLayout()
+    {
+        InputLayout layout = new();
+        layout.Add(new() { Format = ElementFormat.Float3, Semantic = ElementSemantic.Position });
+        layout.Add(new() { Format = ElementFormat.Float3, Semantic = ElementSemantic.Normal });
+        layout.Add(new() { Format = ElementFormat.Float2, Semantic = ElementSemantic.TexCoord });
+        layout.Add(new() { Format = ElementFormat.Float3, Semantic = ElementSemantic.Color });
+
+        return layout;
+    }
+}
