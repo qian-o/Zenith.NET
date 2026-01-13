d77d564304470070163374863cce599b7abcdf26
qian-o <1324771795@qq.com>
2026-01-13 11:35:03 +0800

    Add FrameCoordinator and IZenithView to Views namespace
    Introduce FrameCoordinator for managing render/present timing with dual tasks and synchronization. Add IZenithView interface for standardized view integration, including Output property, GraphicsContext, and RenderRequested event.

---
+++ sources/Views/Zenith.NET.Views/FrameCoordinator.cs
@@ -0,0 +1,69 @@
+﻿using System.Diagnostics;
+
+namespace Zenith.NET.Views;
+
+public class FrameCoordinator(Action<double, double> render, Action present)
+{
+    private readonly Stopwatch renderStopwatch = new();
+    private readonly Stopwatch lifetimeStopwatch = new();
+
+    private CancellationTokenSource? cancellationTokenSource;
+    private AutoResetEvent? renderEvent;
+    private AutoResetEvent? presentEvent;
+
+    public void Start()
+    {
+        Stop();
+
+        renderStopwatch.Start();
+        lifetimeStopwatch.Start();
+
+        cancellationTokenSource = new();
+        renderEvent = new(false);
+        presentEvent = new(true);
+
+        Task.Run(async () =>
+        {
+            while (!cancellationTokenSource.IsCancellationRequested)
+            {
+                presentEvent.WaitOne();
+
+                render(renderStopwatch.Elapsed.TotalSeconds, lifetimeStopwatch.Elapsed.TotalSeconds);
+                renderStopwatch.Restart();
+
+                renderEvent.Set();
+
+                await Task.Yield();
+            }
+        }, cancellationTokenSource.Token);
+
+        Task.Run(async () =>
+        {
+            while (!cancellationTokenSource.IsCancellationRequested)
+            {
+                renderEvent.WaitOne();
+
+                present();
+
+                presentEvent.Set();
+
+                await Task.Delay(16, cancellationTokenSource.Token);
+            }
+        }, cancellationTokenSource.Token);
+    }
+
+    public void Stop()
+    {
+        renderStopwatch.Reset();
+        lifetimeStopwatch.Reset();
+
+        cancellationTokenSource?.Cancel();
+        cancellationTokenSource?.Dispose();
+
+        renderEvent?.Close();
+        renderEvent?.Dispose();
+
+        presentEvent?.Close();
+        presentEvent?.Dispose();
+    }
+}
---
+++ sources/Views/Zenith.NET.Views/IZenithView.cs
@@ -0,0 +1,10 @@
+﻿namespace Zenith.NET.Views;
+
+public interface IZenithView
+{
+    static Output Output { get; }
+
+    GraphicsContext? GraphicsContext { get; set; }
+
+    event EventHandler<RenderEventArgs>? RenderRequested;
+}
