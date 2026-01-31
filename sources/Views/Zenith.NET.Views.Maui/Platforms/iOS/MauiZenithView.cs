43944e79088aa7b284bc2cc50b044b463fa5a076
qian-o <1324771795@qq.com>
2026-01-31 14:39:29 +0800

    Add Metal swapchain support to MauiZenithView (iOS/MacCat)
    Implement CAMetalLayer and swapchain management for MauiZenithView on both iOS and MacCatalyst. This includes dynamic swapchain creation, resizing, and presentation, as well as updated pixel formats for color and depth-stencil attachments. Rendering is now performed only when the graphics context and view size are valid, improving resource management and enabling efficient Metal-based rendering.

--- sources/Views/Zenith.NET.Views.Maui/Platforms/iOS/MauiZenithView.cs
+++ sources/Views/Zenith.NET.Views.Maui/Platforms/iOS/MauiZenithView.cs
@@ -1,29 +1,74 @@
-﻿using UIKit;
+﻿using CoreAnimation;
+using CoreGraphics;
+using Foundation;
+using ObjCRuntime;
+using UIKit;
 
 namespace Zenith.NET.Views.Maui.Platforms.iOS;
 
 internal class MauiZenithView(ZenithViewHandler handler) : UIView
 {
+    private SwapChain? swapChain;
+
     public static Output Output { get; } = new()
     {
-        ColorAttachments = [PixelFormat.R8G8B8A8UNorm],
-        DepthStencilAttachment = PixelFormat.D24UNormS8UInt,
+        ColorAttachments = [PixelFormat.B8G8R8A8UNorm],
+        DepthStencilAttachment = PixelFormat.D32FloatS8UInt,
         SampleCount = SampleCount.Count1
     };
 
     public void EnsureResources()
     {
+        CGSize size = Layer.PreferredFrameSize();
+
+        if (handler.VirtualView.GraphicsContext is null || (uint)size.Height is 0 || (uint)size.Height is 0)
+        {
+            return;
+        }
+
+        uint width = (uint)Math.Clamp(size.Width, 1, uint.MaxValue);
+        uint height = (uint)Math.Clamp(size.Height, 1, uint.MaxValue);
+
+        if (swapChain is null)
+        {
+            swapChain = handler.VirtualView.GraphicsContext.CreateSwapChain(new()
+            {
+                Surface = Surface.Apple(Layer.Handle, width, height),
+                ColorTargetFormat = PixelFormat.B8G8R8A8UNorm,
+                DepthStencilTargetFormat = PixelFormat.D32FloatS8UInt
+            });
+        }
+        else if (swapChain.Desc.Surface.Width != width || swapChain.Desc.Surface.Height != height)
+        {
+            swapChain.Resize(width, height);
+        }
     }
 
     public void Tick()
     {
+        if (swapChain is null)
+        {
+            return;
+        }
+
+        handler.VirtualView.OnUpdateRequested();
+        handler.VirtualView.OnRenderRequested(swapChain.FrameBuffer);
     }
 
     public void Present()
     {
+        swapChain?.Present();
     }
 
     public void ReleaseResources()
     {
+        swapChain?.Dispose();
+        swapChain = null;
+    }
+
+    [Export("layerClass")]
+    public static Class LayerClass()
+    {
+        return new Class(typeof(CAMetalLayer));
     }
 }
