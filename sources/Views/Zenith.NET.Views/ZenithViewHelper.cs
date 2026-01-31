7cdd97237fc516b952cc0dc8ec7148ef25882dea
qian-o <1324771795@qq.com>
2026-01-31 16:36:52 +0800

    Refactor ZenithViewHelper property initialization
    Moved property initialization for ColorTargetFormat and DepthStencilTargetFormat from the static constructor to inline expression-bodied properties. This removes the need for a static constructor and improves code clarity and conciseness.

--- sources/Views/Zenith.NET.Views/ZenithViewHelper.cs
+++ sources/Views/Zenith.NET.Views/ZenithViewHelper.cs
@@ -2,23 +2,9 @@
 
 public static class ZenithViewHelper
 {
-    static ZenithViewHelper()
-    {
-        if (OperatingSystem.IsAndroid())
-        {
-            ColorTargetFormat = PixelFormat.R8G8B8A8UNorm;
-        }
-        else
-        {
-            ColorTargetFormat = PixelFormat.B8G8R8A8UNorm;
-        }
+    public static PixelFormat ColorTargetFormat { get; } = OperatingSystem.IsAndroid() ? PixelFormat.R8G8B8A8UNorm : PixelFormat.B8G8R8A8UNorm;
 
-        DepthStencilTargetFormat = PixelFormat.D32FloatS8UInt;
-    }
-
-    public static PixelFormat ColorTargetFormat { get; }
-
-    public static PixelFormat DepthStencilTargetFormat { get; }
+    public static PixelFormat DepthStencilTargetFormat { get; } = PixelFormat.D32FloatS8UInt;
 
     public static Output Output { get; } = new()
     {
