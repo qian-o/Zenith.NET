fdac2a5b3bb5834626368b6d7824605d6a0166e0
qian-o <1324771795@qq.com>
2026-03-26 00:03:09 +0800

    Refactor window size handling for DPI scaling
    Removed WindowWidth, WindowHeight, and DpiScale properties from the App class. Now, logical width and height are computed on the fly by dividing framebuffer size by DPI scale within the update event. This ensures imGui and camera updates use correct logical dimensions regardless of DPI settings.

--- sources/Experiments/CornellBox/App.cs
+++ sources/Experiments/CornellBox/App.cs
@@ -68,10 +68,6 @@ internal static class App
 
     public static uint Height => (uint)window.FramebufferSize.Y;
 
-    public static uint WindowWidth => (uint)window.Size.X;
-
-    public static uint WindowHeight => (uint)window.Size.Y;
-
     public static Vector2 DpiScale => (Vector2)window.FramebufferSize / (Vector2)window.Size;
 
     public static void Run()
@@ -83,8 +79,11 @@ internal static class App
                 return;
             }
 
-            imGui.Update(delta, WindowWidth, WindowHeight);
-            camera.Update(delta, WindowWidth, WindowHeight);
+            uint width = (uint)(Width / DpiScale.X);
+            uint height = (uint)(Height / DpiScale.Y);
+
+            imGui.Update(delta, width, height);
+            camera.Update(delta, width, height);
         };
 
         window.Render += _ =>
