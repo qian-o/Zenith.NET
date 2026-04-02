b04c3f92a7c3ee1fe6634ab1da55e1cb9721f17c
qian-o <1324771795@qq.com>
2026-04-02 10:09:19 +0800

    Add platform check to restrict app to supported OSes
    Added a check in the App class constructor to throw a PlatformNotSupportedException if the application is run on an OS other than Windows, macOS, or Linux. This prevents execution on unsupported platforms.

--- sources/Experiments/CornellBox/App.cs
+++ sources/Experiments/CornellBox/App.cs
@@ -27,6 +27,11 @@ internal static class App
 
     static App()
     {
+        if (!OperatingSystem.IsWindows() && !OperatingSystem.IsMacOS() && !OperatingSystem.IsLinux())
+        {
+            throw new PlatformNotSupportedException("This application only supports Windows, macOS, and Linux.");
+        }
+
         if (OperatingSystem.IsWindows())
         {
             Context = GraphicsContext.CreateDirectX12(useValidationLayer: true);
