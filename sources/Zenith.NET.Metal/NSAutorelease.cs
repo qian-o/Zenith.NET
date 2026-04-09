521a742e005d0a757d5bb88f91fc55e9bc89b428
qian-o <1324771795@qq.com>
2026-04-09 14:19:01 +0800

    Remove unused Own overloads from NSAutorelease
    Removed the Own<T1, T2, T> and Own<T1, T2, T3, T> methods from the NSAutorelease class. These methods handled object retention with NSAutoreleasePool for functions with two or three arguments, but are no longer needed or have been refactored.

--- sources/Zenith.NET.Metal/NSAutorelease.cs
+++ sources/Zenith.NET.Metal/NSAutorelease.cs
@@ -17,18 +17,4 @@ internal static class NSAutorelease
 
         return func(arg1).Retain();
     }
-
-    public static T Own<T1, T2, T>(Func<T1, T2, T> func, T1 arg1, T2 arg2) where T : NSObject, INativeObject<T>
-    {
-        using NSAutoreleasePool _ = new();
-
-        return func(arg1, arg2).Retain();
-    }
-
-    public static T Own<T1, T2, T3, T>(Func<T1, T2, T3, T> func, T1 arg1, T2 arg2, T3 arg3) where T : NSObject, INativeObject<T>
-    {
-        using NSAutoreleasePool _ = new();
-
-        return func(arg1, arg2, arg3).Retain();
-    }
 }
