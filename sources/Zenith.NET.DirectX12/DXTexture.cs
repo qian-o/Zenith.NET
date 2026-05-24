bcb1712a9f1a07267721f7de47e82053aea23f71
qian-o <1324771795@qq.com>
2026-05-24 14:54:54 +0800

    Implement DXTexture and DXTextureView for DirectX 12
    Added DXTexture and DXTextureView classes to manage DirectX 12 textures and views. Updated DXGraphicsContext to instantiate these classes instead of throwing NotImplementedException. Included resource and descriptor token management, as well as proper cleanup in Destroy methods.

---
+++ sources/Zenith.NET.DirectX12/DXTexture.cs
@@ -0,0 +1,36 @@
+﻿using Silk.NET.Core.Native;
+using Silk.NET.Direct3D12;
+
+namespace Zenith.NET.DirectX12;
+
+internal class DXTexture : Texture
+{
+    public ComPtr<ID3D12Resource> Resource;
+
+    public DXTexture(DXGraphicsContext context, TextureDesc desc) : base(context, desc)
+    {
+    }
+
+    public DXTextureView View { get; }
+
+    public override ResourceHandle SampledHandle => View.SampledHandle;
+
+    public override ResourceHandle StorageHandle => View.StorageHandle;
+
+    public override nint GetNativeObject(NativeObjectType type)
+    {
+        return 0;
+    }
+
+    protected override void SetResourceName(string name)
+    {
+        Resource.SetName(name).Success();
+    }
+
+    protected override void Destroy()
+    {
+        View.Dispose();
+
+        Resource.Dispose();
+    }
+}
---
+++ sources/Zenith.NET.DirectX12/DXTextureView.cs
@@ -0,0 +1,36 @@
+﻿namespace Zenith.NET.DirectX12;
+
+internal class DXTextureView(DXGraphicsContext context, TextureViewDesc desc) : TextureView(context, desc)
+{
+    private DXDescriptorToken? srvToken;
+    private DXDescriptorToken? uavToken;
+
+    public override ResourceHandle SampledHandle => (srvToken ??= CreateSrvToken()).ResourceHandle;
+
+    public override ResourceHandle StorageHandle => (uavToken ??= CreateUavToken()).ResourceHandle;
+
+    public override nint GetNativeObject(NativeObjectType type)
+    {
+        return 0;
+    }
+
+    protected override void SetResourceName(string name)
+    {
+    }
+
+    protected override void Destroy()
+    {
+        uavToken?.Dispose();
+        srvToken?.Dispose();
+    }
+
+    private DXDescriptorToken CreateSrvToken()
+    {
+        throw new NotImplementedException();
+    }
+
+    private DXDescriptorToken CreateUavToken()
+    {
+        throw new NotImplementedException();
+    }
+}
