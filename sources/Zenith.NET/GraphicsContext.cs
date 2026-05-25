ebf482876f25c6da2c21dd071ff36a8a98746fdd
qian-o <1324771795@qq.com>
2026-05-25 16:13:48 +0800

    Remove default null for searchPaths in shader methods
    Removed the default value of null for the searchPaths parameter in CreateShaderFromFile and CreateShaderFromSource. Callers must now explicitly provide this argument.

--- sources/Zenith.NET/GraphicsContext.cs
+++ sources/Zenith.NET/GraphicsContext.cs
@@ -113,7 +113,7 @@ public abstract class GraphicsContext : DisposableObject, INativeObject
         return CreateQueryHeapImpl(desc);
     }
 
-    public Shader CreateShaderFromFile(string file, string entryPoint, ShaderStages stage, string[]? searchPaths = null)
+    public Shader CreateShaderFromFile(string file, string entryPoint, ShaderStages stage, string[]? searchPaths)
     {
         return CreateShader(new()
         {
@@ -123,7 +123,7 @@ public abstract class GraphicsContext : DisposableObject, INativeObject
         });
     }
 
-    public Shader CreateShaderFromSource(string source, string entryPoint, ShaderStages stage, string[]? searchPaths = null)
+    public Shader CreateShaderFromSource(string source, string entryPoint, ShaderStages stage, string[]? searchPaths)
     {
         return CreateShader(new()
         {
