29056635e0c132ffe63313c54b303d5777693a28
qian-o <1324771795@qq.com>
2026-07-03 17:29:20 +0800

    Expand VKMeshShadingPipeline Vulkan pipeline setup
    Refactor constructor to fully configure Vulkan mesh shading
    pipeline, including shader stages, input assembly, attachment
    formats, and dynamic state using ZenithMarshal.Scope.

--- sources/Zenith.NET.Vulkan/VKMeshShadingPipeline.cs
+++ sources/Zenith.NET.Vulkan/VKMeshShadingPipeline.cs
@@ -8,6 +8,69 @@ internal unsafe class VKMeshShadingPipeline : MeshShadingPipeline
 
     public VKMeshShadingPipeline(VKGraphicsContext context, MeshShadingPipelineDesc desc) : base(context, desc)
     {
+        using ZenithMarshal.Scope scope = new();
+
+        GraphicsPipelineCreateInfo createInfo = new()
+        {
+            SType = StructureType.GraphicsPipelineCreateInfo,
+            StageCount = 2,
+            PStages = (PipelineShaderStageCreateInfo*)ZenithMarshal.AllocateAndFill(scope,
+            [
+                desc.MeshShader.Vulkan().GetPipelineShaderStageCreateInfo(scope, ShaderStageFlags.MeshBitExt),
+                desc.FragmentShader.Vulkan().GetPipelineShaderStageCreateInfo(scope, ShaderStageFlags.FragmentBit)
+            ])
+        };
+
+        if (desc.TaskShader is not null)
+        {
+            createInfo.StageCount = 3;
+            createInfo.PStages = (PipelineShaderStageCreateInfo*)ZenithMarshal.AllocateAndFill(scope,
+            [
+                desc.TaskShader.Vulkan().GetPipelineShaderStageCreateInfo(scope, ShaderStageFlags.TaskBitExt),
+                desc.MeshShader.Vulkan().GetPipelineShaderStageCreateInfo(scope, ShaderStageFlags.MeshBitExt),
+                desc.FragmentShader.Vulkan().GetPipelineShaderStageCreateInfo(scope, ShaderStageFlags.FragmentBit)
+            ]);
+        }
+
+        // PrimitiveTopology
+        {
+            PipelineInputAssemblyStateCreateInfo inputAssemblyState = new()
+            {
+                SType = StructureType.PipelineInputAssemblyStateCreateInfo,
+                Topology = VKFormats.Vulkan(desc.PrimitiveTopology)
+            };
+
+            createInfo.PInputAssemblyState = &inputAssemblyState;
+        }
+
+        // AttachmentFormats
+        {
+            PipelineRenderingCreateInfo rendering = new()
+            {
+                SType = StructureType.PipelineRenderingCreateInfo,
+                ColorAttachmentCount = (uint)desc.AttachmentFormats.ColorFormats.Length,
+                PColorAttachmentFormats = (Format*)ZenithMarshal.AllocateAndFill(scope, [.. desc.AttachmentFormats.ColorFormats.Select(static item => VKFormats.Vulkan(item))]),
+                DepthAttachmentFormat = VKFormats.Vulkan(desc.AttachmentFormats.DepthStencilFormat ?? PixelFormat.Unknown).Format,
+                StencilAttachmentFormat = VKFormats.Vulkan(desc.AttachmentFormats.DepthStencilFormat ?? PixelFormat.Unknown).Format
+            };
+
+            createInfo.PNext = &rendering;
+        }
+
+        // RenderState
+        {
+        }
+
+        PipelineDynamicStateCreateInfo dynamicState = new()
+        {
+            SType = StructureType.PipelineDynamicStateCreateInfo,
+            DynamicStateCount = 4,
+            PDynamicStates = (DynamicState*)ZenithMarshal.AllocateAndFill(scope, [DynamicState.Viewport, DynamicState.Scissor, DynamicState.BlendConstants, DynamicState.StencilReference])
+        };
+
+        createInfo.PDynamicState = &dynamicState;
+
+        context.Vk.CreateGraphicsPipelines(context.Device, default, 1, &createInfo, default, out Pipeline).Success();
     }
 
     public new VKGraphicsContext Context => (VKGraphicsContext)base.Context;
