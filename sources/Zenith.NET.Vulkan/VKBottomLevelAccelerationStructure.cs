cc73a2b7fe15f59e3ab09e9d68727df6a7073808
qian-o <1324771795@qq.com>
2026-07-06 15:34:10 +0800

    Add Vulkan BLAS creation, update, and device address support
    Expanded VKBottomLevelAccelerationStructure to allocate buffers,
    create and build the acceleration structure, and query its device
    address. Added Update method for in-place geometry updates.
    Stubbed Info method for geometry/build range preparation.

--- sources/Zenith.NET.Vulkan/VKBottomLevelAccelerationStructure.cs
+++ sources/Zenith.NET.Vulkan/VKBottomLevelAccelerationStructure.cs
@@ -6,8 +6,58 @@ internal unsafe class VKBottomLevelAccelerationStructure : BottomLevelAccelerati
 {
     public AccelerationStructureKHR AccelerationStructure;
 
+    public ulong DeviceAddress;
+
     public VKBottomLevelAccelerationStructure(VKGraphicsContext context, VKCommandBuffer commandBuffer, BottomLevelAccelerationStructureDesc desc) : base(context, desc)
     {
+        using ZenithMarshal.Scope scope = new();
+
+        Transform = new(context, new()
+        {
+            SizeInBytes = (uint)(sizeof(TransformMatrixKHR) * desc.Geometries.Length),
+            Residency = MemoryResidency.CpuWriteOnly
+        }, BufferUsageFlags.AccelerationStructureBuildInputReadOnlyBitKhr);
+
+        AccelerationStructureBuildGeometryInfoKHR info = Info(scope, desc, out uint* maxPrimitiveCounts, out AccelerationStructureBuildRangeInfoKHR* buildRangeInfos);
+
+        AccelerationStructureBuildSizesInfoKHR sizeInfo = new() { SType = StructureType.AccelerationStructureBuildSizesInfoKhr };
+        context.AccelerationStructure?.GetAccelerationStructureBuildSizes(context.Device, AccelerationStructureBuildTypeKHR.DeviceKhr, &info, maxPrimitiveCounts, &sizeInfo);
+
+        Storage = new(context, new()
+        {
+            SizeInBytes = (uint)sizeInfo.AccelerationStructureSize,
+            Residency = MemoryResidency.GpuOnly
+        }, BufferUsageFlags.AccelerationStructureStorageBitKhr);
+
+        Scratch = new(context, new()
+        {
+            SizeInBytes = (uint)sizeInfo.BuildScratchSize,
+            Usages = BufferUsages.StorageReadWrite,
+            Residency = MemoryResidency.GpuOnly
+        });
+
+        AccelerationStructureCreateInfoKHR createInfo = new()
+        {
+            SType = StructureType.AccelerationStructureCreateInfoKhr,
+            Buffer = Storage.Buffer,
+            Size = sizeInfo.AccelerationStructureSize,
+            Type = AccelerationStructureTypeKHR.BottomLevelKhr
+        };
+
+        context.AccelerationStructure?.CreateAccelerationStructure(context.Device, &createInfo, default, out AccelerationStructure).Success();
+
+        AccelerationStructureDeviceAddressInfoKHR deviceAddressInfo = new()
+        {
+            SType = StructureType.AccelerationStructureDeviceAddressInfoKhr,
+            AccelerationStructure = AccelerationStructure
+        };
+
+        DeviceAddress = context.AccelerationStructure?.GetAccelerationStructureDeviceAddress(context.Device, &deviceAddressInfo) ?? 0;
+
+        info.DstAccelerationStructure = AccelerationStructure;
+        info.ScratchData = new() { DeviceAddress = Scratch.DeviceAddress };
+
+        context.AccelerationStructure?.CmdBuildAccelerationStructures(commandBuffer.CommandBuffer, 1, &info, &buildRangeInfos);
     }
 
     public new VKGraphicsContext Context => (VKGraphicsContext)base.Context;
@@ -20,6 +70,15 @@ internal unsafe class VKBottomLevelAccelerationStructure : BottomLevelAccelerati
 
     public void Update(VKCommandBuffer commandBuffer, BottomLevelAccelerationStructureDesc newDesc)
     {
+        using ZenithMarshal.Scope scope = new();
+
+        AccelerationStructureBuildGeometryInfoKHR info = Info(scope, newDesc, out uint* maxPrimitiveCounts, out AccelerationStructureBuildRangeInfoKHR* buildRangeInfos);
+        info.Mode = BuildAccelerationStructureModeKHR.UpdateKhr;
+        info.SrcAccelerationStructure = AccelerationStructure;
+        info.DstAccelerationStructure = AccelerationStructure;
+        info.ScratchData = new() { DeviceAddress = Scratch.DeviceAddress };
+
+        Context.AccelerationStructure?.CmdBuildAccelerationStructures(commandBuffer.CommandBuffer, 1, &info, &buildRangeInfos);
     }
 
     public override nint GetNativeObject(NativeObjectType type)
@@ -49,5 +108,10 @@ internal unsafe class VKBottomLevelAccelerationStructure : BottomLevelAccelerati
         Scratch.Dispose();
         Storage.Dispose();
         Transform.Dispose();
+    }
+
+    private AccelerationStructureBuildGeometryInfoKHR Info(ZenithMarshal.Scope scope, BottomLevelAccelerationStructureDesc desc, out uint* maxPrimitiveCounts, out AccelerationStructureBuildRangeInfoKHR* buildRangeInfos)
+    {
+        throw new NotImplementedException();
     }
 }
